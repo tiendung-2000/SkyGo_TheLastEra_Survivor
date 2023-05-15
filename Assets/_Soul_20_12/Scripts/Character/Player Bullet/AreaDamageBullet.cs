39eb6a602a277e763ea969bdc93c45da547b5c23
Ngo Tien Dung <tiendung2000tn@gmail.com>
2023-05-15 17:21:48 +0700

    data

---
+++ Assets/_Soul_20_12/Scripts/Character/Player Bullet/AreaDamageBullet.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaDamageBullet : MonoBehaviour
+{
+    public float speed = 15f;
+    public Rigidbody2D theRB;
+
+    public GameObject areaDamage;
+    public int damageToGive = 50;
+
+    public TrailRenderer trail;
+
+    private void OnDisable()
+    {
+        trail.Clear();
+    }
+
+    void Update()
+    {
+        theRB.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Spawn impact effect
+        Instantiate(areaDamage, transform.position, transform.rotation);
+
+        // Check the tag of the collided object
+        switch (other.tag)
+        {
+            case "Enemy":
+                // Apply damage to the enemy
+                EnemyController enemy = other.GetComponent<EnemyController>();
+                if (enemy != null)
+                {
+                    enemy.DamageEnemy(damageToGive);
+                }
+                break;
+            case "Boss":
+                // Apply damage to the boss and spawn hit effect
+                BossController boss = other.GetComponent<BossController>();
+                if (boss != null)
+                {
+                    boss.TakeDamage(damageToGive);
+                    Instantiate(boss.hitEffect, transform.position, transform.rotation);
+                }
+                break;
+        }
+        SmartPool.Ins.Despawn(gameObject);
+    }
+}
